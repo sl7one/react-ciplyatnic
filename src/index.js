33d7cc1248f23c803975be3708c3fbad84d19cf9
Slava <110696118+sl7one@users.noreply.github.com>
2023-03-06 19:05:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter basename="/cyplyatnic">
+  <BrowserRouter basename="/react-ciplyatnic">
     <Provider store={store}>
       <App />
     </Provider>
